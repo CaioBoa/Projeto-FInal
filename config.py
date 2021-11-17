b47eb5ddcc438c112751fb636334deac88d76397
CaioBoa <caioob@al.insper.edu.br>
2021-11-17 10:27:59 -0300

    config

---
+++ config.py
@@ -0,0 +1,11 @@
+from os import path
+
+#caminho para as pastas de som, imagem e fonte
+IMG_DIR = path.join(path.dirname(__file__), 'assets', 'img')
+SND_DIR = path.join(path.dirname(__file__), 'assets', 'snd')
+FNT_DIR = path.join(path.dirname(__file__), 'assets', 'font')
+
+# configurações gerais
+WIDTH = 480 
+HEIGHT = 600 
+FPS = 60 
