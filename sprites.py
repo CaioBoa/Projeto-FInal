4c1af9f903f02dc4d93b35d1175eefa076c25cad
CaioBoa <caioob@al.insper.edu.br>
2021-11-17 17:51:14 -0300

    Física do Player + Gerador de asteróides em posições aleatórias

--- sprites.py
+++ sprites.py
@@ -1,7 +1,7 @@
 import random
 import pygame
-from config import WIDTH, HEIGHT
-from assets import CHARACTER
+from config import CHAR_HEIGHT, CHAR_WIDTH, WIDTH, HEIGHT, AST_HEIGHT
+from assets import CHARACTER, ASTEROID
 
 class character(pygame.sprite.Sprite):
     def __init__(self,groups,assets):
@@ -10,17 +10,44 @@ class character(pygame.sprite.Sprite):
         self.image = assets[CHARACTER]
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH / 2
-        self.rect.bottom = HEIGHT - 10
+        self.rect.x = WIDTH / 2 - CHAR_WIDTH / 2
+        self.rect.y = HEIGHT - CHAR_HEIGHT 
         self.speedy = 0
         self.groups = groups
         self.assets = assets
     
     def update(self):
 
-        self.rect.y += self.speedy
+        self.rect.y -= self.speedy
 
-        if self.rect.top > 0:
-            self.rect.top = 0
-        if self.rect.bottom < HEIGHT:
-            self.rect.bottom = HEIGHT
+        if self.rect.y < 0:
+            self.rect.y = 0
+        if self.rect.y > HEIGHT - CHAR_HEIGHT:
+            self.rect.y = HEIGHT - CHAR_HEIGHT
+        if self.rect.y < HEIGHT - CHAR_HEIGHT and self.speedy == 0:
+            self.speedy = -3
+
+class asteroid(pygame.sprite.Sprite):
+    def __init__(self,groups,assets):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = assets[ASTEROID]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        randomy = random.randint(AST_HEIGHT + 5 , HEIGHT - AST_HEIGHT - 5)
+        self.rect.x = WIDTH - 50
+        self.rect.y = randomy
+        self.speedx = 2
+        self.groups = groups
+        self.assets = assets
+    
+    def update(self):
+
+        self.rect.x -= self.speedx
+
+        if self.rect.x < 0:
+            self.kill()
+
+    
+
+
