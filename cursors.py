87b9ede0ef65062f1750332801b8257ce82ef5bc
CaioBoa <caioob@al.insper.edu.br>
2021-12-01 16:03:14 -0300

    Cursor tentando resolver bug

--- cursors.py
+++ cursors.py
@@ -7,8 +7,4 @@ bitmap_2 = pygame.cursors.Cursor(
 (24, 24), (0, 0), *pygame.cursors.compile(pygame.cursors.thickarrow_strings)
 )
 
-surf = pygame.Surface((40, 40)) # you could also load an image 
-surf.fill((120, 50, 50))        # and use that as your surface
-color = pygame.cursors.Cursor((20, 20), surf)
-
-cursors = [system, bitmap_1, bitmap_2, color]
+cursors = [system, bitmap_1, bitmap_2]
